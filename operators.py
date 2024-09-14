5be2a87b5a00c6febbf75cf120a4ee4579fc0c9f
Jon Francis <francisjon64@gmail.com>
2024-09-14 10:07:31 -0400

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,12 @@
+print(7 + 7)
+print(8*9)
+#stand division 
+print(9/3)
+#integer division
+print(7//2) #this will print the wholest low number 
+#modulus division 
+print(6%2) #you can divide this just fine, and nothing will be left over. 
+print(7%2) #this is for another example of what will be left over. 
+#power operator
+#** is like saying - to the power of blank *
+print(3**3)
