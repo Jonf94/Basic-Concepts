badbc9d29e6ffb69fab3e63e99a376c1a0e51b32
Jon Francis <francisjon64@gmail.com>
2024-09-14 11:12:05 -0400

    Create reassigning_values.py

---
+++ reassigning_values.py
@@ -0,0 +1,14 @@
+age = 20
+print(age)
+
+age = 20
+print(age + 5)
+
+age = 20
+age += 7 #this is another way you can write it 
+print(age)
+
+#you can do the same thing with *, - , /
+# there something called Concatenation that someone can utlize as well. 
+text = 'jonwesley' + 'francis'
+print(text)
