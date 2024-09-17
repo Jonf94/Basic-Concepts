9b6c9b7dc2204c115cda4469851b3ce599f0213f
Jon Francis <francisjon64@gmail.com>
2024-09-17 07:22:23 -0400

    Create More_operators.py

---
+++ More_operators.py
@@ -0,0 +1,11 @@
+#bianry operators: consist of two operants. an example is 5 - 4 or 5 + 7
+#unary operaators, use only 1 operant. 
+#print(+12)
+
+#now to learn, the order of operators. 
+# 1. **
+# 2. */ // %
+# 3. +-
+print((3 + 2) * 3)
+
+print(0.1 +0.1 + 0.1)
