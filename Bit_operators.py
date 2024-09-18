a3b378c333ac6c9494ed89530ac40fb7e4596538
Jon Francis <francisjon64@gmail.com>
2024-09-18 19:07:01 -0400

    Create Bit_operators.py

---
+++ Bit_operators.py
@@ -0,0 +1,33 @@
+first_bit = 1
+second_bit = 0
+
+print(first_bit & second_bit)
+
+
+
+first_bit = 1
+second_bit = 0
+
+print(first_bit | second_bit)
+
+
+
+
+first_bit = 1
+second_bit = 0
+
+print(first_bit ^ second_bit)
+
+
+
+print(~0)
+
+
+
+
+print(12<<2)
+
+
+
+
+print(12>>2)
