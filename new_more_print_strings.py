da68fb638bc9cf8925b047f6f12625edf14c2dc4
Jon Francis <francisjon64@gmail.com>
2024-09-18 18:49:11 -0400

    Create new_more_print_strings.py

---
+++ new_more_print_strings.py
@@ -0,0 +1,9 @@
+print(len('hey there'))
+#this is classified as a key word argument
+print('hey, jon', end='.')
+print('this is java speaking!')
+#another term that you need to know is the "sep" argument. 
+#sep is indicated by "sep='-'"
+
+first_name = 'mark'
+print('my first name is', first_name, 'Welcome', sep='-')
