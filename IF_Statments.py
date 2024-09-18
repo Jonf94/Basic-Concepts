441e8f8ef5d43d0958f689f4d1ee4cd88d8a23f2
Jon Francis <francisjon64@gmail.com>
2024-09-18 19:38:25 -0400

    Create IF_Statments.py

---
+++ IF_Statments.py
@@ -0,0 +1,9 @@
+user_age = int(input("what is you age ma'am!"))
+if user_age > 30:
+    print('you are OOOVERRRR 30')
+elif user_age == 30:
+    print('you are exactly 30 years olldddd')
+    print('you will need to make more additional in order to qaulify.')
+else:
+    print('you are 30 years old or younnnnger')
+    print('CONGRADUATIONS, YOU QUALIFY')
