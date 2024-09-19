e9d38a8ab85f9bbefb859188073313a44c3d9545
Jon Francis <francisjon64@gmail.com>
2024-09-19 08:03:50 -0500

    Create logical_Op_Condit.py

---
+++ logical_Op_Condit.py
@@ -0,0 +1,29 @@
+#Avail logical Operators
+
+# < less than
+# > Greater than 
+# <= less than or equal to 
+# >= Greater than or equal to 
+# == Equals 
+# != not equal to
+
+#lines 11 - 15 are correct
+#password = input('Do you know the secret password')
+#if password != '--secret':
+#    print('not correct')
+#else:
+#    print('correct password')
+
+
+if True:
+    print('condition met')
+
+if False:
+    print('conditiion met')
+
+
+if 1 != 2:
+    print(True)
+
+if 2 == 2.0:
+    print('true')
