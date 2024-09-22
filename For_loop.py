d793231f2e0c522e4fab39b21836e532ee591ce7
Jon Francis <francisjon64@gmail.com>
2024-09-22 02:46:52 -0500

    Create For_loop.py

---
+++ For_loop.py
@@ -0,0 +1,9 @@
+for letter in 'goodbye':
+    print('current letter:', letter)
+
+for counter in range(1,11):
+    print(counter)
+
+#below will generate every 4th number. 
+for counter in range(1,11,4):
+    print(counter)
