111466ab5474ac838b27388f5db02457e6ec4db3
Jon Francis <francisjon64@gmail.com>
2024-09-23 06:55:10 -0500

    Create into+lists.py

---
+++ into+lists.py
@@ -0,0 +1,24 @@
+#things imporatant to remember 
+#Lists.
+#tuples.
+#dictionaires. 
+
+city_1 = 'New York City'
+city_2 = 'Los angeles'
+city_3 = 'Chicago'
+city_4 = 'Houston'
+city_5 = 'Phoenix'
+
+empty_List = []
+top_cities = ['New York City', 'Los angeles','Chicago','Houston','Phoenix']
+print(top_cities)
+
+#keep in mind that indexing starts with zero, and connects/counts to whatever is the first paramater listed is.
+print(top_cities[3])
+
+#using negative(-1] correspond the list from read from right to lest. 
+print(top_cities[-2])
+
+print(top_cities[0:3])
+
+print(top_cities[:])
